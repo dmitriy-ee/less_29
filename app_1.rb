d8b05108ca7e6d55f8139195bff94346bc26e007
dmitriy_ee <dmitriy_ee@hotmail.com>
2017-12-22 00:32:56 +0200

    visit.erb edit

--- app_1.rb
+++ app_1.rb
@@ -12,7 +12,35 @@ end
 class Barbers < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barbers.all
+end
+
 get '/' do
-	@barbers = Barbers.all 
+	#@barbers = Barbers.all
+	@barbers = Barbers.order "created_at DESC" 
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barbers = params[:barbers]
+
+	hh =    {
+			:username => 'Please, enter your name',
+			:phone => 'Please, enter your phone', 
+			:datetime => 'Please, enter visit day & time'
+			}
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join("; ")
+
+	if @error != ''
+		return erb :visit
+	end
 end
